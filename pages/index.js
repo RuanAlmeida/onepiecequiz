93c8542601888eb25e6598cd45fae48a103cebc2
Ruan Almeida da Silva <ruan.silva@basis.com.br>
2021-01-25 23:01:59 -0300

    first day

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,54 @@
-import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+import styled from 'styled-components'
+import db from '../db.json';
+import Widget from '../src/components/Widget'
+import QuizLogo from '../src/components/QuizLogo'
+import QuizBackground from '../src/components/QuizBackground'
+import Footer from '../src/components/footer'
+import GitHubCorner from '../src/components/GitHubCorner'
+
+// const BackgroundImage = styled.div`
+//   background-image: url(${db.bg});
+//   flex: 1;
+//   background-size: cover;
+//   background-position: color-interpolation-filters;
+// `;
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 550px) {
+    margin: auto;
+    padding: 15px;
+  }
 `
 
+
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+    <QuizContainer>
+      <QuizLogo />
+        <Widget>
+          <Widget.Header>
+          <h1>One Piece</h1>
+          </Widget.Header>
+          <Widget.Content>
+          <p>testestestseteste.,...</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+        <Widget.Content>
+           <h1>Quiz One Piece</h1>
+          <p>testestestseteste.,...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/RuanAlmeida" />
+    </QuizBackground>
+  );
 }
